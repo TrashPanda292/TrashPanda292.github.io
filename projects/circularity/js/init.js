8b998a592abbf01367605e526ec143ebf7ee19a6
Trash Panda <mrsword293@gmail.com>
2022-04-06 13:48:00 +0000

    finshed

--- projects/circularity/js/init.js
+++ projects/circularity/js/init.js
@@ -27,22 +27,26 @@ var init = function (window) {
         // TODO 2 : Create a function that draws a circle 
         function drawCircle(){
             circle = draw.randomCircleInArea(canvas, true, true, '#999', 2);
-            physikz.addRandomVelocity(circle, canvas);
+            physikz.addRandomVelocity(circle, canvas, 100, 100);
             view.addChild(circle);
             circles.push(circle);
         }
 
         // TODO 3 / 8 : Call the drawCircle() function 
-        drawCircle()
-        physikz.addRandomVelocity(circle, canvas, 10, 10);
-        drawCircle();
-        physikz.addRandomVelocity(circle, canvas, 10, 10);
-        drawCircle();
-        physikz.addRandomVelocity(circle, canvas, 10, 10);
-        drawCircle();
-        physikz.addRandomVelocity(circle, canvas, 10, 10);
-        drawCircle();
-        physikz.addRandomVelocity(circle, canvas, 10, 10);
+        // drawCircle()
+        // physikz.addRandomVelocity(circle, canvas, 10, 10);
+        // drawCircle();
+        // physikz.addRandomVelocity(circle, canvas, 10, 10);
+        // drawCircle();
+        // physikz.addRandomVelocity(circle, canvas, 10, 10);
+        // drawCircle();
+        // physikz.addRandomVelocity(circle, canvas, 10, 10);
+        // drawCircle();
+        // physikz.addRandomVelocity(circle, canvas, 10, 10);
+        for (var i = 0; i < 100; i++){
+            drawCircle();
+          
+        }
 
         ////////////////////////////////////////////////////////////
         ///////////////// PROGRAM LOGIC ////////////////////////////
@@ -55,27 +59,31 @@ var init = function (window) {
         */
         function update() {
             // TODO 4 : Update the circle's position //
-            physikz.updatePosition(circles[0]);
-            physikz.updatePosition(circles[1]);
-            physikz.updatePosition(circles[2]);
-            physikz.updatePosition(circles[3]);
-            physikz.updatePosition(circles[4]);
+            // physikz.updatePosition(circles[0]);
+            // physikz.updatePosition(circles[1]);
+            // physikz.updatePosition(circles[2]);
+            // physikz.updatePosition(circles[3]);
+            // physikz.updatePosition(circles[4]);
+            
 
 
 
             
             // TODO 5 / 10 : Call game.checkCirclePosition() on your circles.
-            game.checkCirclePosition(circles[0]);
-            game.checkCirclePosition(circles[1]);
-            game.checkCirclePosition(circles[2]);
-            game.checkCirclePosition(circles[3]);
-            game.checkCirclePosition(circles[4]);
-
-
+            // game.checkCirclePosition(circles[0]);
+            // game.checkCirclePosition(circles[1]);
+            // game.checkCirclePosition(circles[2]);
+            // game.checkCirclePosition(circles[3]);
+            // game.checkCirclePosition(circles[4]);
+console.log(circle.length)
             // TODO 9 : Iterate over the array
-           
-            
-        }
+            for (var i = 0; i < circles.length; i++){
+              var eachCircle = circles[i]
+              physikz.updatePosition(eachCircle)
+              game.checkCirclePosition(eachCircle)
+            }
+              
+       }
     
         /* 
         This Function should check the position of a circle that is passed to the 
